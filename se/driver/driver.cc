4f39a9b111c42b96998d8649f5d5931579255804
cwz920716 <cwz920716@gmail.com>
2016-11-18 00:56:38 -0600

    top level

--- se/driver/driver.cc
+++ se/driver/driver.cc
@@ -1,12 +1,15 @@
 #include "se/stream_executor/lib/demangle.h"
+#include "se/stream_executor/blas.h"
 #include <iostream>
 
 using namespace std;
 
 using perftools::gputools::port::Demangle;
+using namespace perftools::gputools::blas;
 
 int main() {
   std::cout << Demangle("St13bad_exception") << std::endl;
+  std::cout << TransposeString(Transpose::kTranspose) << std::endl;
   std::cout << "Hello World!" << std::endl;
   return 0;
 }
